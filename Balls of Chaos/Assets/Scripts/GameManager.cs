408e48d75b43a24112f44b67a650e75846b16069
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2023-04-06 20:16:29 +0300

    Сделал таймер

--- Balls of Chaos/Assets/Scripts/GameManager.cs
+++ Balls of Chaos/Assets/Scripts/GameManager.cs
@@ -4,33 +4,58 @@ using UnityEngine;
 using TMPro;
 using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GoalScript blue, red, orange, green;
-    private TextMeshProUGUI gameOverText;
-    private bool isGameOver;
-    
+    [SerializeField] private GoalScript _blue, _red, _orange, _green;
+    private TextMeshProUGUI _gameOverText;
+    private TextMeshProUGUI _timerText;
+    private bool _isGameOver;
+    private bool _onTimer;
+    private float _time;
+
     // Start is called before the first frame update
     void Start()
     {
-        gameOverText = GameObject.Find("Game Over Text").GetComponent<TextMeshProUGUI>();
-        gameOverText.gameObject.SetActive(false);
+       _onTimer = true;
+
+        _gameOverText = GameObject.Find("Game Over Text").GetComponent<TextMeshProUGUI>();
+        _gameOverText.gameObject.SetActive(false);
+
+        _timerText = GameObject.Find("Timer").GetComponent<TextMeshProUGUI>();
+        _timerText.text = _time.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGameOver = blue.isSolved && red.isSolved && orange.isSolved && green.isSolved;
+        Timing();
 
-        if (isGameOver)
+        _isGameOver = _blue.isSolved && _red.isSolved && _orange.isSolved && _green.isSolved;
+
+        if (_isGameOver)
         {
-            gameOverText.gameObject.SetActive(true);
+            _gameOverText.gameObject.SetActive(true);
+            _onTimer = false;
+            _gameOverText.text = "You win! Your result - " + Mathf.Round(_time).ToString() + " second.";
         }
     }
+
+
 
     public void Restart(int scene)
     {
         SceneManager.LoadScene(scene);
+    }
+
+    private void Timing()
+    {
+        if (_onTimer)
+        {
+            _time += Time.deltaTime;
+        }
+
+        _timerText.text = Mathf.Round(_time).ToString();
     }
 }
